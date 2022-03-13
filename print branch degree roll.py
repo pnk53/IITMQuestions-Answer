63545dfb936b829cc98abe5f2bb1741932200d9c
Prashik <prashikkamble.pnk54@gmail.com>
2022-03-13 20:22:47 +0530

    10th que

---
+++ print branch degree roll.py
@@ -0,0 +1,14 @@
+print('Enter your email id: ')
+email = str(input())
+
+branch = email[0:2]
+degree = email[3:5]
+year = email[6:8]
+roll = email[9:13]
+
+print(branch)
+print(degree)
+print(year)
+print(roll)
+
+# print(branch, degree, year, roll, sep='\n')
