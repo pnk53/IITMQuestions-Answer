7a8c57aed6d1853eeb7bac3cfa1c954b915c7625
Prashik <prashikkamble.pnk54@gmail.com>
2022-03-15 16:52:32 +0530

    16th que

---
+++ print time of day.py
@@ -0,0 +1,13 @@
+print('Enter an Integer: ')
+t = int(input())
+
+if (0<=t<=5):
+    print('NIGHT')
+elif (6<=t<=11):
+    print('MORNING')
+elif (12<=t<=17):
+    print('AFTERNOON')
+elif (18<=t<=23):
+    print('EVENING')
+else:
+    print('INVALID')
