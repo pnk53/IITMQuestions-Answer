aa2937fc0abaff80da609a10410c1705dc9ded6e
Prashik <prashikkamble.pnk54@gmail.com>
2022-03-24 17:17:16 +0530

    22nd que

---
+++ Print lesser greatest and bigger smallest.py
@@ -0,0 +1,6 @@
+print('Enter a real number: ')
+n = float(input())
+i = int(n)
+smallest = i + 1
+print(f'Smallest integer greater than is {smallest}')
+print(f'Greatest integer smaller than is {i}')
